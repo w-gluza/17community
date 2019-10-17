fd4429c87969141f904c9cfccebba46eb852235d
Wiola-work <wiolagluza@gmail.com>
2019-10-17 22:04:15 +0200

    current user profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,29 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
 
-// @route   GET api/profile
-// @desc    Test route
-// @access  Public
-router.get("/", (request, response) => response.send("Profile route"));
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
+// @route   GET api/profile/me
+// @desc    Get current users profile
+// @access  Private
+router.get('/me', auth, async (request, response) => {
+  try {
+    const profile = await Profile.findOne({ user: request.user.id }).populate(
+      'user',
+      ['name', 'avatar'],
+    );
+    if (!profile) {
+      return response
+        .status(400)
+        .json({ msg: 'The specified user does not have a profile' });
+    }
+    response.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    response.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
