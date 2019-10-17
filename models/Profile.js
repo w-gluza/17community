ec30a2850e2a0c4d564b3b88cea31e9f053b5ee7
Wiola-work <wiolagluza@gmail.com>
2019-10-17 21:44:23 +0200

    user profile fields

---
+++ models/Profile.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const ProfileSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+  },
+  website: {
+    type: String,
+  },
+  location: {
+    type: String,
+  },
+  status: {
+    type: String,
+  },
+  // interests = skills
+  interests: {
+    type: [String],
+  },
+  bio: {
+    type: String,
+  },
+  instagramusername: {
+    type: String,
+  },
+  // social media connections
+  social: {
+    instagram: {
+      type: String,
+    },
+    facebook: {
+      type: String,
+    },
+    linkedin: {
+      type: String,
+    },
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+module.exports = Profile = mongoose.model('profile', ProfileSchema);
