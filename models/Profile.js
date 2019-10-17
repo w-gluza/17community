cec1bb6e3dbdd392f341a27d63a8c3331a64910a
Izabela Turaj <i.turaj@gmail.com>
2019-10-17 21:55:44 +0200

    fixing merge conflict

--- models/Profile.js
+++ models/Profile.js
@@ -1,8 +1,4 @@
 const mongoose = require('mongoose');
-<<<<<<< HEAD
-
-=======
->>>>>>> ec30a2850e2a0c4d564b3b88cea31e9f053b5ee7
 const ProfileSchema = new mongoose.Schema({
   user: {
     type: mongoose.Schema.Types.ObjectId,
@@ -44,8 +40,4 @@ const ProfileSchema = new mongoose.Schema({
     default: Date.now,
   },
 });
-<<<<<<< HEAD
-
-=======
->>>>>>> ec30a2850e2a0c4d564b3b88cea31e9f053b5ee7
 module.exports = Profile = mongoose.model('profile', ProfileSchema);
