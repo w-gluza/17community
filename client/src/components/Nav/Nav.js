8004be8af9e7cd5062e72ea0c86603e3b48c831c
Wioletta Gluza <wiolagluza@gmail.com>
2019-10-21 21:27:24 +0200

    React Router integration

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Nav = () => {
   return (
     <>
-      <nav>Nav</nav>
+      <nav>
+        <h1>
+          <Link to='/'>17 Community</Link>
+        </h1>
+        <ul>
+          <li>
+            <Link to='/members'>Members</Link>
+          </li>
+          <li>
+            <Link to='/register'>Register</Link>
+          </li>
+          <li>
+            <Link to='/login'>Login</Link>
+          </li>
+        </ul>
+      </nav>
     </>
   );
 };
---
+++ client/src/components/Register/Register.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Register() {
+  return (
+    <>
+      <p>Register Form</p>
+    </>
+  )
+}
+
+export default Register
