38da39202348353c9bee4df41cd0090b48e0ec27
Izabela Turaj <i.turaj@gmail.com>
2019-10-21 20:54:24 +0200

    basic structure

---
+++ client/src/components/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <>
+      <main>Main</main>
+    </>
+  );
+};
+
+export default Home;
---
+++ client/src/components/Nav/Nav.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Nav = () => {
+  return (
+    <>
+      <nav>Nav</nav>
+    </>
+  );
+};
+
+export default Nav;
