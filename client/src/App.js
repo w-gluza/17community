5bd135b181dbcb897b4ef0605c9a57191b24156b
Izabela Turaj <i.turaj@gmail.com>
2019-10-26 22:05:57 +0200

    redux setup

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import './main.scss';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
+
 // components import
 import Home from './components/Home/Home';
 import Nav from './components/Nav/Nav';
@@ -9,16 +13,18 @@ import Login from './components/Login/Login';
 import Register from './components/Register/Register';
 
 const App = () => (
-  <Router>
-    <Nav />
-    <Route exact path='/' component={Home} />
-    <main>
-      <Switch>
-        <Route exact path='/Login' component={Login} />
-        <Route exact path='/register' component={Register} />
-      </Switch>
-    </main>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Nav />
+      <Route exact path='/' component={Home} />
+      <main>
+        <Switch>
+          <Route exact path='/Login' component={Login} />
+          <Route exact path='/register' component={Register} />
+        </Switch>
+      </main>
+    </Router>
+  </Provider>
 );
 
 export default App;
