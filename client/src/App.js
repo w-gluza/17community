745c1076382a23b2cba4590e7000b70514a8a46a
Wioletta Gluza <wiolagluza@gmail.com>
2019-11-12 00:35:19 +0100

    implementing private route

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,21 @@
-import React, { useEffect } from 'react';
-import './main.scss';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React, { useEffect } from "react";
+import "./main.scss";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 // Redux
-import { Provider } from 'react-redux';
-import store from './store';
-import { loadUser } from './actions/auth';
-import setAuthToken from './utilities/setAuthToken';
+import { Provider } from "react-redux";
+import store from "./store";
+import { loadUser } from "./actions/auth";
+import setAuthToken from "./utilities/setAuthToken";
 
 // components import
-import Home from './components/Home/Home';
-import Nav from './components/Nav/Nav';
-import Login from './components/Login/Login';
-import Register from './components/Register/Register';
-import Alert from './components/Alert/Alert';
+import Home from "./components/Home/Home";
+import Nav from "./components/Nav/Nav";
+import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
+import Alert from "./components/Alert/Alert";
+import Dashboard from "./components/Dashboard/Dashboard";
+import PrivateRoute from "./components/Routing/PrivateRoute";
 
 if (localStorage.token) {
   setAuthToken(localStorage.token);
@@ -28,12 +30,13 @@ const App = () => {
     <Provider store={store}>
       <Router>
         <Nav />
-        <Route exact path='/' component={Home} />
+        <Route exact path="/" component={Home} />
         <main>
           <Alert />
           <Switch>
-            <Route exact path='/Login' component={Login} />
-            <Route exact path='/register' component={Register} />
+            <Route exact path="/Login" component={Login} />
+            <Route exact path="/register" component={Register} />
+            <PrivateRoute exact path="/dashboard" component={Dashboard} />
           </Switch>
         </main>
       </Router>
---
+++ client/src/components/Routing/PrivateRoute.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import PropTypes from "prop-types";
+
+import { connect } from "react-redux";
+
+// Implementing private route for the app that will be accessible
+// for only authenticated userSelect
+const PrivateRoute = ({
+  component: Component,
+  auth: { isAuthenticated, loading },
+  ...rest
+}) => (
+  <Route
+    {...rest}
+    render={props =>
+      !isAuthenticated && !loading ? (
+        <Redirect to="/login" />
+      ) : (
+        <Component {...props} />
+      )
+    }
+  />
+);
+
+PrivateRoute.propTypes = {
+  auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(mapStateToProps)(PrivateRoute);
