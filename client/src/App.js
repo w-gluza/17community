82247cc1ed3206d9d847ccaa4769108dc12885d8
Wioletta Gluza <wiolagluza@gmail.com>
2019-12-03 20:01:03 +0100

    profile error fixed

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,27 @@
-import React, { useState, useEffect } from 'react';
-import './main.scss';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import "./main.scss";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 // Redux
-import { Provider } from 'react-redux';
-import store from './store';
-import { loadUser } from './actions/auth';
-import setAuthToken from './utilities/setAuthToken';
+import { Provider } from "react-redux";
+import store from "./store";
+import { loadUser } from "./actions/auth";
+import setAuthToken from "./utilities/setAuthToken";
 
 // components import
-import Home from './components/Home/Home';
-import Authentication from './components/Authentication/Authentication';
-import Nav from './components/Nav/Nav';
-import Alert from './components/Alert/Alert';
-import Dashboard from './components/Dashboard/Dashboard';
-import CreateProfile from './components/CreateProfile/CreateProfile';
-import EditProfile from './components/EditProfile/EditProfile';
-import Profile from './components/Profile/Profile';
-import Profiles from './components/Profiles/Profiles';
-import Posts from './components/Posts/Posts';
-import Post from './components/Post/Post';
-import PrivateRoute from './components/Routing/PrivateRoute';
-import Milestones from './components/Milestones/Milestones';
+import Home from "./components/Home/Home";
+import Authentication from "./components/Authentication/Authentication";
+import Nav from "./components/Nav/Nav";
+import Alert from "./components/Alert/Alert";
+import Dashboard from "./components/Dashboard/Dashboard";
+import CreateProfile from "./components/CreateProfile/CreateProfile";
+import EditProfile from "./components/EditProfile/EditProfile";
+import Profile from "./components/Profile/Profile";
+import Profiles from "./components/Profiles/Profiles";
+import Posts from "./components/Posts/Posts";
+import Post from "./components/Post/Post";
+import PrivateRoute from "./components/Routing/PrivateRoute";
+import Milestones from "./components/Milestones/Milestones";
 
 if (localStorage.token) {
   setAuthToken(localStorage.token);
@@ -42,32 +42,32 @@ const App = () => {
         <Switch>
           <Route
             exact
-            path='/authentication'
+            path="/authentication"
             render={() => <Route component={Authentication} />}
           />
-          <div className='page-container'>
+          <div className="page-container">
             <Nav
               isMenuOpen={isMenuOpen}
               onToggleMenu={() => setIsMenuOpen(!isMenuOpen)}
             />
-            <Route exact path='/' component={Home} />
-            <main className='main'>
-              <Route exact path='/profiles:id' component={Profile} />
-              <Route exact path='/profiles' component={Profiles} />
-              <Route exact path='/milestones' component={Milestones} />
-              <PrivateRoute exact path='/dashboard' component={Dashboard} />
+            <Route exact path="/" component={Home} />
+            <main className="main">
+              <Route exact path="/profile/:id" component={Profile} />
+              <Route exact path="/profiles" component={Profiles} />
+              <Route exact path="/milestones" component={Milestones} />
+              <PrivateRoute exact path="/dashboard" component={Dashboard} />
               <PrivateRoute
                 exact
-                path='/create-profile'
+                path="/create-profile"
                 component={CreateProfile}
               />
               <PrivateRoute
                 exact
-                path='/edit-profile'
+                path="/edit-profile"
                 component={EditProfile}
               />
-              <PrivateRoute exact path='/posts' component={Posts} />
-              <PrivateRoute exact path='/posts/:id' component={Post} />
+              <PrivateRoute exact path="/posts" component={Posts} />
+              <PrivateRoute exact path="/posts/:id" component={Post} />
             </main>
           </div>
         </Switch>
