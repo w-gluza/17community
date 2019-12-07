60e33fde7ed7c73e77985517273eefe511c9b2b4
Wioletta Gluza <wiolagluza@gmail.com>
2019-12-07 18:45:09 +0100

    picture upload

--- client/src/components/PostForm/PostForm.js
+++ client/src/components/PostForm/PostForm.js
@@ -4,23 +4,52 @@ import { connect } from "react-redux";
 import { addPost } from "../../actions/post";
 
 const PostForm = ({ addPost }) => {
-  const [text, setText] = useState("");
+  const [formData, setFormData] = useState({
+    text: "",
+    img: ""
+  });
+  const { text, img } = formData;
+
+  const onChange = e =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = e => {
+    e.preventDefault();
+    const toBase64 = file =>
+      new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => resolve(reader.result);
+        reader.onerror = error => reject(error);
+      });
+    async function Main() {
+      const file = e.target.img.files[0];
+      console.log(await toBase64(file));
+      formData.img = await toBase64(file);
+      await addPost(formData);
+    }
+
+    Main();
+    console.log(formData);
+  };
 
   return (
     <article>
       <p>Please write your post below</p>
-      <form
-        onSubmit={e => {
-          e.preventDefault();
-          addPost({ text });
-          setText("");
-        }}
-      >
+      <form onSubmit={e => onSubmit(e)}>
+        <input
+          accept="image/*"
+          id="inputGroupFile01"
+          type="file"
+          name="img"
+          value={img}
+          onChange={e => onChange(e)}
+        />
         <textarea
           name="text"
           value={text}
           placeholder="Add something cool"
-          onChange={e => setText(e.target.value)}
+          onChange={e => onChange(e)}
           required
         ></textarea>
         <button type="submit">Submit</button>
@@ -28,9 +57,7 @@ const PostForm = ({ addPost }) => {
     </article>
   );
 };
-
 PostForm.propTypes = {
   addPost: PropTypes.func.isRequired
 };
-
 export default connect(null, { addPost })(PostForm);
