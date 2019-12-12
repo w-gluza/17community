8779c0e8fa63326eeaa40d609ea43285c0f8e389
Izabela Turaj <i.turaj@gmail.com>
2019-12-12 22:40:53 +0100

    adds upload image

--- client/src/components/PostForm/PostForm.js
+++ client/src/components/PostForm/PostForm.js
@@ -1,21 +1,21 @@
-import React, { useState } from "react";
-import PropTypes from "prop-types";
-import { connect } from "react-redux";
-import { addPost } from "../../actions/post";
-import postImg from "../../assets/illustrations/00_community.svg";
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { addPost } from '../../actions/post';
+import postImg from '../../assets/illustrations/upload.svg';
 // Material-UI Components Imports
-import light from "../../themes/light";
-import { ThemeProvider } from "@material-ui/styles";
-import Card from "@material-ui/core/Card";
-import Button from "@material-ui/core/Button";
-import { TextField } from "@material-ui/core";
-import CloudUploadIcon from "@material-ui/icons/CloudUpload";
+import light from '../../themes/light';
+import { ThemeProvider } from '@material-ui/styles';
+import Card from '@material-ui/core/Card';
+import Button from '@material-ui/core/Button';
+import { TextField } from '@material-ui/core';
+import CloudUploadIcon from '@material-ui/icons/CloudUpload';
 
 const PostForm = ({ addPost }) => {
   const [formData, setFormData] = useState({
-    title: "",
-    text: "",
-    img: ""
+    title: '',
+    text: '',
+    img: '',
   });
   const { title, text, img } = formData;
   console.log(formData);
@@ -43,59 +43,57 @@ const PostForm = ({ addPost }) => {
 
   return (
     <ThemeProvider theme={light}>
-      <article className="post__article">
-        <Card className="post__card">
-          <figure className="post__figure">
-            <img src={postImg} className="post_img" alt="postimg"></img>
+      <article className='post__article'>
+        <Card className='post__card'>
+          <figure className='post__figure'>
+            <img src={postImg} className='post_img' alt='postimg'></img>
           </figure>
-          <form onSubmit={e => onSubmit(e)} className="post__form">
-            <h4>Please write your post below</h4>
+          <form onSubmit={e => onSubmit(e)} className='post__form'>
+            <h4>Spread the word</h4>
             <TextField
-              label="Post Title"
-              name="title"
+              label='Title'
+              name='title'
               value={title}
-              placeholder="Title"
+              placeholder='Title'
               onChange={e => onChange(e)}
-              margin="normal"
-              variant="outlined"
+              margin='normal'
+              variant='outlined'
               required
             />
             <TextField
-              label="Post Content"
-              variant="outlined"
-              name="text"
+              label='Content'
+              variant='outlined'
+              name='text'
               value={text}
-              placeholder="Add something cool"
+              placeholder='Add something cool'
               onChange={e => onChange(e)}
               required
             />
             <input
-              name="img"
-              accept="image/*"
-              style={{ display: "none" }}
-              id="postImg"
+              name='img'
+              accept='image/*'
+              style={{ display: 'none' }}
+              id='postImg'
               multiple
-              type="file"
+              type='file'
               value={img}
               onChange={e => onChange(e)}
             />
-            <label htmlFor="postImg">
+            <label htmlFor='postImg'>
               <Button
-                component="span"
-                variant="contained"
-                color="default"
-                startIcon={<CloudUploadIcon />}
-              >
+                component='span'
+                variant='contained'
+                color='default'
+                startIcon={<CloudUploadIcon />}>
                 Upload
               </Button>
             </label>
             <Button
-              type="submit"
-              className="button"
-              variant="contained"
-              color="primary"
-              size="large"
-            >
+              type='submit'
+              className='button'
+              variant='contained'
+              color='primary'
+              size='large'>
               Submit
             </Button>
           </form>
@@ -105,6 +103,6 @@ const PostForm = ({ addPost }) => {
   );
 };
 PostForm.propTypes = {
-  addPost: PropTypes.func.isRequired
+  addPost: PropTypes.func.isRequired,
 };
 export default connect(null, { addPost })(PostForm);
