7a08c03f977e87ac6b1e3d3c5be3d8456c896679
Izabela Turaj <i.turaj@gmail.com>
2019-12-12 23:38:55 +0100

    media queries

--- client/src/components/Comment/Comment.js
+++ client/src/components/Comment/Comment.js
@@ -1,28 +1,28 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import PropTypes from "prop-types";
-import { connect } from "react-redux";
-import Moment from "react-moment";
-import { deleteComment } from "../../actions/post";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Moment from 'react-moment';
+import { deleteComment } from '../../actions/post';
 
 const Comment = ({
   postId,
   comment: { _id, name, user, avatar, text, date },
   auth,
-  deleteComment
+  deleteComment,
 }) => (
   <div>
     <Link to={`/profile/${user}`}>
-      <img src={avatar} alt="avatar" />
+      <img src={avatar} alt='avatar' />
       <p>{name}</p>
     </Link>
     <p>{text}</p>
     <p>
-      Posted on: <Moment format="YYYY/MM/DD">{date}</Moment>
+      Posted on: <Moment format='YYYY/MM/DD'>{date}</Moment>
     </p>
 
     {!auth.loading && user === auth.user._id && (
-      <button onClick={() => deleteComment(postId, _id)} type="button">
+      <button onClick={() => deleteComment(postId, _id)} type='button'>
         DELETE COMMENT
       </button>
     )}
@@ -33,11 +33,11 @@ Comment.propTypes = {
   deleteComment: PropTypes.func.isRequired,
   postId: PropTypes.string.isRequired,
   comment: PropTypes.object.isRequired,
-  auth: PropTypes.object.isRequired
+  auth: PropTypes.object.isRequired,
 };
 
 const mapStateToProps = state => ({
-  auth: state.auth
+  auth: state.auth,
 });
 
 export default connect(mapStateToProps, { deleteComment })(Comment);
--- client/src/components/CommentForm/CommentForm.js
+++ client/src/components/CommentForm/CommentForm.js
@@ -1,36 +1,57 @@
-import React, { useState } from "react";
-import PropTypes from "prop-types";
-import { connect } from "react-redux";
-import { addComment } from "../../actions/post";
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { addComment } from '../../actions/post';
+import spreadLove from '../../assets/illustrations/love.svg';
+
+// Material-UI Components Imports
+import light from '../../themes/light';
+import { ThemeProvider } from '@material-ui/styles';
+import Card from '@material-ui/core/Card';
+import Button from '@material-ui/core/Button';
+import { TextField } from '@material-ui/core';
 
 const CommentForm = ({ postId, addComment }) => {
-  const [text, setText] = useState("");
+  const [text, setText] = useState('');
 
   return (
-    <article>
-      <p>Comment</p>
-      <form
-        onSubmit={e => {
-          e.preventDefault();
-          addComment(postId, { text });
-          setText("");
-        }}
-      >
-        <textarea
-          name="text"
-          value={text}
-          placeholder="Add some cool comment"
-          onChange={e => setText(e.target.value)}
-          required
-        ></textarea>
-        <button type="submit">Submit</button>
-      </form>
-    </article>
+    <ThemeProvider theme={light}>
+      <Card className='comments'>
+        <form
+          className='comments-form'
+          onSubmit={e => {
+            e.preventDefault();
+            addComment(postId, { text });
+            setText('');
+          }}>
+          <p>Comment</p>
+          <TextField
+            className='comment-field'
+            name='text'
+            variant='outlined'
+            value={text}
+            placeholder='Spread some love'
+            onChange={e => setText(e.target.value)}
+            required></TextField>
+          <Button
+            type='submit'
+            className='button'
+            variant='contained'
+            color='primary'
+            size='large'>
+            Submit
+          </Button>
+        </form>
+        <figure>
+          <img src={spreadLove} alt='spread love' />
+        </figure>
+      </Card>
+    </ThemeProvider>
   );
 };
 
 CommentForm.propTypes = {
-  addComment: PropTypes.func.isRequired
+  addComment: PropTypes.func.isRequired,
 };
 
 export default connect(null, { addComment })(CommentForm);
